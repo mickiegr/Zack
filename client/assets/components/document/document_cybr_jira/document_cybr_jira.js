cd7cda2f03f99a10fc435436bbad4165b07c69c9
Eyal Raz <eyal.raz@cybereason.com>
2018-07-04 20:15:37 +0300

    added cyber-jira document. need to style it and add the appropriate logic for it. figure out how to group results together

---
+++ client/assets/components/document/document_cybr_jira/document_cybr_jira.js
@@ -0,0 +1,53 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('lucidworksView.components.document_cybr_jira', ['lucidworksView.services.signals'])
+    .directive('documentCybrJira', documentCybrJira);
+
+  function documentCybrJira() {
+    'ngInject';
+    var directive = {
+      restrict: 'EA',
+      templateUrl: 'assets/components/document/document_cybr_jira/document_cybr_jira.html',
+      scope: true,
+      controller: Controller,
+      controllerAs: 'vm',
+      bindToController: {
+        doc: '=',
+        highlight: '='
+      }
+    };
+
+    return directive;
+
+  }
+
+  function Controller(DocumentService) {
+    'ngInject';
+    var vm = this;
+    var templateFields = Object.keys(vm.doc);
+    vm.getTemplateDisplayFieldName = getTemplateDisplayFieldName;
+
+    activate();
+
+    function activate() {
+      vm.doc = processDocument(vm.doc);
+    }
+
+    function processDocument(doc) {
+      //set properties needed for display
+      doc._templateDisplayFields = DocumentService.setTemplateDisplayFields(doc, templateFields);
+      doc._templateDisplayFields._lw_id_decoded = DocumentService.decodeFieldValue(doc._templateDisplayFields, 'id');
+
+      //set properties needed for signals
+      doc._signals = DocumentService.setSignalsProperties(doc, vm.position);
+
+      return doc;
+    }
+
+    function getTemplateDisplayFieldName(field){
+      return DocumentService.getTemplateDisplayFieldName(vm.doc, field);
+    }
+  }
+})();
